f56b07339be9e947333abd21fae05f0aa9c6b7dd
Joona <t9hejo01@students.oamk.fi>
2023-10-29 21:26:41 +0200

    Implemented Payment service

--- ToriApp/Server/Controllers/AuthController.cs
+++ ToriApp/Server/Controllers/AuthController.cs
@@ -58,5 +58,6 @@ namespace ToriApp.Server.Controllers
 
             return Ok(respomse);
         }
+
     }
 }
---
+++ ToriApp/Server/Interfaces/IPaymentService.cs
@@ -0,0 +1,10 @@
+﻿using Stripe.Checkout;
+
+namespace ToriApp.Server.Interfaces
+{
+    public interface IPaymentService
+    {
+        Task<Session> CreateCheckoutSession();
+        Task<ServiceResponse<bool>> FulfillOrder(HttpRequest request);
+    }
+}
