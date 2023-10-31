48079dce11c315ac480293b78a0d3c5460ebd78c
Joona <t9hejo01@students.oamk.fi>
2023-10-31 17:31:39 +0200

    Refactoring

--- ToriApp/Shared/User.cs
+++ ToriApp/Shared/User.cs
@@ -14,6 +14,6 @@ namespace ToriApp.Shared
         public byte[] PasswordSalt { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
         public Address Address { get; set; }
-        public string Role { get; set; } = "Customer";
+        
     }
 }
